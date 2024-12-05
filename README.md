015fd84caae1d5e6fbcaed3c8496287442475d53
Skyler Forge <strictlyskyler@gmail.com>
2024-12-04 18:51:00 -0800

    Frontend routing

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 Time spent:
 1. Design & thinking ~(like a long-running cron)
 2. Setup & Scaffolding ~30min (`89318e3`)
+3. Routing ~30min (`bba60e0`)
 
 TODO:
 1. Design & Thinking
 2. ~~Scaffolding~~
-3. UI Routing
+3. ~~Routing~~
 4. List, Add, Edit Page forms
 5. Add member functionality
 6. Delete member
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,87 @@
 import React from 'react';
 import './App.css';
 import axios from 'axios';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useSearchParams,
+} from 'react-router-dom';
 
 
-class App extends React.Component{
+function List (props) {
+  return (
+    <div>
+      <header>
+        <h1>Game Night Team Members</h1>
+      </header>
+      <Link to="/add">+</Link>
+      <hr></hr>
+      {props.members.map((member, id) => (
+        <div key={id}>
+          <Link to={`/edit?id=${id}`}>
+            <ul>
+              <li>{member.first_name}</li>
+              <li>{member.last_name}</li>
+              <li>{member.email}</li>
+              <li>{member.phone}</li>
+            </ul>
+          </Link>
+        </div>
+      ))}
+    </div>
+  )
+}
 
+function Add (props) {
+  return (
+    <div>
+      <header>
+        <h1>Add Team Member</h1>
+      </header>
+      <hr></hr>
+      {props.members.map((member, id) => (
+        <div key={id}>
+          <ul>
+            <li>{member.first_name}</li>
+            <li>{member.last_name}</li>
+            <li>{member.email}</li>
+            <li>{member.phone}</li>
+          </ul>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+function Edit (props) {
+  const [searchParams] = useSearchParams();
+  const id = parseInt(searchParams.get('id'), 10);
+  const member = props.members[id];
+
+  return (
+    <div>
+      <header>
+        <h1>Edit Team Member</h1>
+      </header>
+      <hr></hr>
+      <div>
+        {!member && <h2>Loading...</h2>}
+        {member && <ul>
+          <li>{member.first_name}</li>
+          <li>{member.last_name}</li>
+          <li>{member.email}</li>
+          <li>{member.phone}</li>
+        </ul>}
+      </div>
+    </div>
+  )
+}
+
+export default class App extends React.Component {
   state = {
-    details: [],
+    members: [],
   };
    
   componentDidMount(){
@@ -17,7 +92,7 @@ class App extends React.Component{
     .then(res => {
       data = res.data;
       this.setState({
-        details: data
+        members: data
       });
     })
     .catch(err => { })
@@ -25,27 +100,15 @@ class App extends React.Component{
   }
 
 
-  render(){
+  render() {
     return (
-      <div>
-        <header>
-          <h1>Game Night Team Members</h1>
-        </header>
-        <hr></hr>
-        {this.state.details.map((output, id) => (
-          <div key={id}>
-            <ul>
-              <li>{output.first_name}</li>
-              <li>{output.last_name}</li>
-              <li>{output.email}</li>
-              <li>{output.phone}</li>
-            </ul>
-          </div>
-        ))}
-      </div>
+      <Router>
+        <Routes>
+          <Route path="/add" element={<Add members={this.state.members} />} />
+          <Route path="/edit" element={<Edit members={this.state.members} />} />
+          <Route path="/" element={<List members={this.state.members} />} />
+        </Routes>
+      </Router>
     )
   }
-
-}
-
-export default App;
+};
